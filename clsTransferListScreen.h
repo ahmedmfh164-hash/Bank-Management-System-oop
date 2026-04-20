f887bf8225f146ee94a786647011eedb849092fe
Ahmed Mohamed <ahmedmfh164@gmail.com>
2026-04-20 17:08:15 +0200

    Update clsTransferListScreen.h

--- clsTransferListScreen.h
+++ clsTransferListScreen.h
@@ -9,14 +9,14 @@ private:
 
     static void _PrintTransferRecord(clsBankClient::stTransfer Transfer)
     {
-        clsUtil::SetColor(2);
-        cout << setw(4) << left << "" << "| " << setw(18) << left << Transfer.DateTime;
-        cout <<"| "<< setw(15) << left << Transfer.AccountNumberFrom;
-        cout << "| " << left << setw(16) << Transfer.AccountBalanceFrom;
-        cout << "| " << setw(14) << left << Transfer.AccountNumberTo;
-        cout << "| " << left << setw(16) << Transfer.AccountBalanceTo;
-        printf("| %.f", Transfer.Amount);
-        cout <<"| "<< left << setw(8) << CurrentUser.UserName;
+         clsUtil::SetColor(2);
+  cout << setw(3) << left << "" << "| " << setw(20) << left << Transfer.DateTime;
+  cout <<"| "<< setw(15) << left << Transfer.AccountNumberFrom;
+  cout << "| " << left << setw(16) << Transfer.AccountBalanceFrom;
+  cout << "| " << setw(14) << left << Transfer.AccountNumberTo;
+  cout << "| " << left << setw(16) << Transfer.AccountBalanceTo;
+  cout << "| " << left << setw(12) <<Transfer.Amount;
+  cout <<"| "<< left << setw(8) << CurrentUser.UserName;
     }
 
 public:
@@ -31,31 +31,31 @@ public:
 
         _DrawScreenHeader(Title, SubTitle);
 
-        cout << setw(4) << left << "" << "=================================================";
-        cout << "===================================================================\n";
-        cout << setw(4) << left << "" << "| " << left << setw(18) <<"Date Time" <<"| "<<
-            left << setw(15) << "Account Number1";
-        cout << "| " << left << setw(16) << "Account Balance1";
-        cout << "| " << left << setw(14) << "AccountNumber2";
-        cout << "| " << left << setw(16) << "Account Balance2";
-        cout << "| " << left << setw(12) << "Amount";
-        cout << "| " << left << setw(13) << "User Name";
-        cout << "\n" << setw(4) << left << "" << "=================================================";
-        cout << "===================================================================\n";
+       cout << setw(3) << left << "" << "=================================================";
+cout << "=====================================================================\n";
+cout << setw(3) << left << "" << "| " << left << setw(20) <<"Date Time" <<"| "<<
+    left << setw(15) << "Account Number1";
+cout << "| " << left << setw(16) << "Account Balance1";
+cout << "| " << left << setw(14) << "AccountNumber2";
+cout << "| " << left << setw(16) << "Account Balance2";
+cout << "| " << left << setw(12) << "Amount";
+cout << "| " << left << setw(13) << "User Name";
+cout << "\n" << setw(3) << left << "" << "=================================================";
+cout << "=====================================================================\n";
 
-        if (vTransfer.size() == 0)
-        {
-            cout << "\n\t\t\tNo Transfers Available In the System!\n";
-        }
-        else
-            for (clsBankClient::stTransfer Transfer : vTransfer)
-            {
-                _PrintTransferRecord(Transfer);
-                cout << endl;
-            }
-        clsUtil::SetColor(11);
-        cout << setw(4) << left << "" << "-----------------------------------------------------";
-        cout << "---------------------------------------------------------------\n";
+if (vTransfer.size() == 0)
+{
+    cout << "\n\t\t\tNo Transfers Available In the System!\n";
+}
+else
+    for (clsBankClient::stTransfer Transfer : vTransfer)
+    {
+        _PrintTransferRecord(Transfer);
+        cout << endl;
+    }
+clsUtil::SetColor(11);
+cout << setw(3) << left << "" << "-----------------------------------------------------";
+cout << "-----------------------------------------------------------------\n";
 
         cout << "\n";
 
